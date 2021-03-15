f99ad72670edaf8b9bf8260b80f1d15d8f027397
MateyR <MateyR@MateyR>
2021-03-15 10:16:56 -0700

    Tarea.1- Se dio funcionalidad al boton "Clean"

--- src/main/java/ni/edu/uni/programacion/controllers/PnlCalculadoraController.java
+++ src/main/java/ni/edu/uni/programacion/controllers/PnlCalculadoraController.java
@@ -7,6 +7,7 @@ package ni.edu.uni.programacion.controllers;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.event.AncestorListener;
 import ni.edu.uni.programacion.views.panels.PnlCalculadora;
 
 /**
@@ -16,6 +17,7 @@ import ni.edu.uni.programacion.views.panels.PnlCalculadora;
 public class PnlCalculadoraController implements ActionListener {
     private PnlCalculadora pnlCalculadora;
 
+   
     public PnlCalculadoraController(PnlCalculadora pnlCalculadora) {
         this.pnlCalculadora = pnlCalculadora;
         initComponent();
@@ -23,18 +25,54 @@ public class PnlCalculadoraController implements ActionListener {
     }
 
     private void initComponent() {
+        /*pnlCalculadora.getBtnCalc().addActionListener(new java.awt.event.ActionListener(){
+            @Override
+            public void actionPerformed(ActionEvent e) {                         // Action Listener es una interfaz que contiene un 1Solo metodo actionperform(ActionEvent e)
+                if (e.getActionCommand().equalsIgnoreCase("Calcular")) {         //Entonces este metodo tomo como parametro un action event que es con el que se trabaja       
+            double n1, n2;                                                       // "Por logica No ahaha" ahora en este apartado se podria hacer al momento en que esta desde el init ccomponents 
+                                                                                 // Pero eso lo volveria desosdenado  entonces hacemos los metodos fuera del init components
+            n1 = Double.parseDouble(pnlCalculadora.getTxtNumber1().getText());   // De hecho neatbeans haci lo hace por defecto
+            n2 = Double.parseDouble(pnlCalculadora.getTxtNumber2().getText());
+
+            //pnlCalculadora.getTxtResult().setText(String.valueOf(suma(n1, n2)));
+            pnlCalculadora.getTxtResult().setText(String.valueOf(n1+ n2));
+        }
+                
+        }
+            
+        }); */ 
+     
+        /*pnlCalculadora.getBtnNew().addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                //btnNewActionPerformed(evt);
+                 if(evt.getActionCommand().equalsIgnoreCase("Nuevo")){
+                 pnlCalculadora.getTxtNumber1().setText("");
+                 pnlCalculadora.getTxtNumber2().setText("");
+                 pnlCalculadora.getTxtResult().setText("");
+            
+        }
+               
+            }
+        });*/
+          
         pnlCalculadora.getBtnCalc().addActionListener(this);  //This para decir que es la variable de instancia  No la variable local, Ademas es la clase la que implementa la
+        pnlCalculadora.getBtnNew().addActionListener(new java.awt.event.ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {           
+            btnNewActionPerformed(e);          }
+        });
     }
 
-    @Override
+    //@Override
     public void actionPerformed(ActionEvent e) {
         if (e.getActionCommand().equalsIgnoreCase("Calcular")) {
             double n1, n2;
+          
             n1 = Double.parseDouble(pnlCalculadora.getTxtNumber1().getText());
             n2 = Double.parseDouble(pnlCalculadora.getTxtNumber2().getText());
 
             //pnlCalculadora.getTxtResult().setText(String.valueOf(suma(n1, n2)));
-            pnlCalculadora.getTxtResult().setText(String.valueOf(n1 + n2));
+            pnlCalculadora.getTxtResult().setText(String.valueOf(n1+ n2));
         }
     }
 
@@ -42,6 +80,21 @@ public class PnlCalculadoraController implements ActionListener {
         return a + b;
     }
     
+      
     
+    private void btnNewActionPerformed(java.awt.event.ActionEvent evt) {                                         
+        if(evt.getActionCommand().equalsIgnoreCase("Nuevo")){
+            pnlCalculadora.getTxtNumber1().setText("");
+            pnlCalculadora.getTxtNumber2().setText("");
+            pnlCalculadora.getTxtResult().setText("");
+            
+        }
+    } 
+
+
+    
+     
+    
+
 
 }
