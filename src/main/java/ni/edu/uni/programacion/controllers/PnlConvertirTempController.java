7460d02060f1fe102d822e39fb90b8bcd42aa9a3
MateyR <MateyR@MateyR>
2021-03-16 20:17:38 -0700

    Tarea 1. Se agrego metodo de validacion de entrada

--- src/main/java/ni/edu/uni/programacion/controllers/PnlConvertirTempController.java
+++ src/main/java/ni/edu/uni/programacion/controllers/PnlConvertirTempController.java
@@ -5,8 +5,11 @@
  */
 package ni.edu.uni.programacion.controllers;
 
+import java.awt.Color;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import javax.swing.BorderFactory;
+import javax.swing.JOptionPane;
 import ni.edu.uni.programacion.views.panels.PnlConversionesTemp;
 
 /**
@@ -30,6 +33,8 @@ public class PnlConvertirTempController implements ActionListener {
 
     @Override
     public void actionPerformed(ActionEvent e) {
+        pnlconversionestemp.getTxttemperatura().setBorder(BorderFactory.createLineBorder(Color.BLACK));
+        Validaciones(pnlconversionestemp);
         
         int index = pnlconversionestemp.getjComboBox1().getSelectedIndex();
         
@@ -69,11 +74,51 @@ public class PnlConvertirTempController implements ActionListener {
             
         }
         
+       
+  
+}
+    
+    
+     private void Validaciones(PnlConversionesTemp conversiontemplate){
+        if(conversiontemplate.getTxttemperatura().getText().isEmpty()){
+            JOptionPane.showMessageDialog(conversiontemplate,
+                    "El campo: VALOR es requerido",
+                    "Mensaje de error",
+                    JOptionPane.ERROR_MESSAGE);
+            conversiontemplate.getTxttemperatura().setBorder(BorderFactory.createLineBorder(Color.RED, 1));
+         
+            return;
+        }
+        
+        String cadena = conversiontemplate.getTxttemperatura().getText();
+        if(isNumeric(cadena) == false) {
+            
+            JOptionPane.showMessageDialog(conversiontemplate,
+                    "El valor ingresado contiene letras",
+                    "Mensaje de error",
+                    JOptionPane.ERROR_MESSAGE);
+            conversiontemplate.getTxttemperatura().setBorder(BorderFactory.createLineBorder(Color.RED, 1));
+           
+        }
+ 
     }
+    
+    private static boolean isNumeric(String cadena){
+	try {
+		Integer.parseInt(cadena);
+		return true;
+	} catch (NumberFormatException nfe){
+		return false;
+	}
+    }
+}
+        
+        
+  
     
     
     
         
     
     
-}
+
